309879da3894b660ef848ea5394d3ae7d63f526c
unknown <kushangtanawal@gmail.com>
2024-02-10 18:26:08 +0530

    first commit

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,65 +1,80 @@
-function populateStates() {
-  $.ajax({
-    url: "	https://data.covid19india.org/v4/min/data.min.json",
-    method: "GET",
-    success: function (data) {
-      const states = Object.keys(data);
-      const stateSelect = $("#stateSelect");
-      stateSelect.empty();
-      stateSelect.append(`<option value="" selected disabled>Select State</option>`);
-      states.forEach(state => {
-        stateSelect.append(`<option value="${state}">${state}</option>`);
-      });
-    }
-  });
-}
+  function fetchStatesData() {
+    $.ajax({
+      url: "https://data.covid19india.org/v4/min/data.min.json",
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        var states = Object.keys(data);
+        var stateOptions = "<option value=''>Select State</option>";
+        states.forEach(function (state) {
+          stateOptions += "<option value='" + state + "'>" + state + "</option>";
+        });
+        $("#stateSelect").html(stateOptions);
+      },
+    });
+  }
 
-function populateCities(state) {
-  $.ajax({
-    url: `	https://data.covid19india.org/v4/min/data.min.json`,
-    method: "GET",
-    success: function (data) {
-      const cities = Object.keys(data[state].districts);
-      const citySelect = $("#citySelect");
-      citySelect.empty();
-      citySelect.append(`<option value="" selected disabled>Select City</option>`);
-      cities.forEach(city => {
-        citySelect.append(`<option value="${city}">${city}</option>`);
-      });
-    }
-  });
-}
+  function fetchCitiesData(state) {
+    $.ajax({
+      url: "https://data.covid19india.org/v4/min/data.min.json",
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        var cities = Object.keys(data[state].districts);
+        var cityOptions = "<option value=''>Select City</option>";
+        cities.forEach(function (city) {
+          cityOptions += "<option value='" + city + "'>" + city + "</option>";
+        });
+        $("#citySelect").html(cityOptions);
+      },
+    });
+  }
 
+  function fetchDailyData(state, city) {
+    $.ajax({
+      url: "https://data.covid19india.org/v4/min/data.min.json",
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        var dailyData;
+        if (city) {
+          dailyData = data[state].districts[city];
+          $("#cityData").
+            text(
+              "Tested: " + dailyData.total.tested +
+              ", Confirmed: " + dailyData.total.confirmed +
+              ", Deceased: " + dailyData.total.deceased +
+              ", Recovered: " + dailyData.total.recovered);
+        }
+        else {
+          dailyData = data[state];
+          $("#stateData").
+            text(
+              "Tested: " + dailyData.total.tested +
+              ", Confirmed: " + dailyData.total.confirmed +
+              ", Deceased: " + dailyData.total.deceased +
+              ", Recovered: " + dailyData.total.recovered);
+        }
 
-function fetchCovidStats(state, city) {
-  $.ajax({
-    url: `	https://data.covid19india.org/v4/min/data.min.json`,
-    method: "GET",
-    success: function (data) {
-      const stats = data[state].districts[city].total;
-      $("#tested").text(stats.tested);
-      $("#dailyConfirmed").text(stats.confirmed);
-      $("#dailyDeceased").text(stats.deceased);
-      $("#dailyRecovered").text(stats.recovered);
-    }
-  });
-}
+      }
 
-$(document).ready(function () {
-  populateStates();
+    });
+  }
+
+  fetchStatesData();
 
   $("#stateSelect").change(function () {
-    const selectedState = $(this).val();
+    var selectedState = $(this).val();
     if (selectedState) {
-      populateCities(selectedState);
+      fetchCitiesData(selectedState);
+      fetchDailyData(selectedState, null);
     }
   });
 
   $("#citySelect").change(function () {
-    const selectedState = $("#stateSelect").val();
-    const selectedCity = $(this).val();
+    var selectedState = $("#stateSelect").val();
+    var selectedCity = $(this).val();
     if (selectedState && selectedCity) {
-      fetchCovidStats(selectedState, selectedCity);
+      fetchDailyData(selectedState, selectedCity);
     }
   });
-});
